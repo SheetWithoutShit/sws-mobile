f7d94054e5f1e976a5f9b63529d2d5fa91fb5ef0
PetrushynskyiOleksii <petrushynskyi.oleksii@gmail.com>
2020-11-04 09:19:59 +0200

    SLM-80: Implement loading indicator

---
+++ app/components/LoadingIndicator/LoadingIndicator.js
@@ -0,0 +1,25 @@
+import React from "react"
+import { View, ActivityIndicator, Platform } from "react-native"
+
+import COLORS from "@utils/colors";
+
+import styles from "./style"
+
+
+const INDICATOR_SIZE = Platform.select({
+  ios: "large",
+    android: 70,
+})
+const LoadingIndicator = ({ }) => {
+
+    return (
+        <View style={styles.container}>
+            <ActivityIndicator
+                size={INDICATOR_SIZE}
+                color={COLORS.gold}
+            />
+        </View>
+    )
+}
+
+export default LoadingIndicator
