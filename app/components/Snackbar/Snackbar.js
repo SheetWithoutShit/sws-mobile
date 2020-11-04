955cf4af9cf007d0b5ed11b9e4631d6905136c5b
PetrushynskyiOleksii <petrushynskyi.oleksii@gmail.com>
2020-11-04 12:10:17 +0200

    SLM-66: Implement snackbars

---
+++ app/components/Snackbar/Snackbar.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react"
+import { Text } from "react-native"
+
+import Icon from "@utils/icon"
+import Button from "@components/Buttons/Button"
+
+import styles from "./style"
+
+
+const SNACKBAR_DURATIONS = {
+    "short": 3000,
+    "long": 10000,
+    "infinity": 0,
+}
+const SNACKBAR_ICONS = {
+    "success": <Icon name="success-circle"/>,
+    "error": <Icon name="error-circle"/>,
+}
+
+
+const Snackbar = ({ visible, text, level, duration, handlePress }) => {
+    const [hide, setHide] = useState(false)
+
+    const hideTime = SNACKBAR_DURATIONS[duration]
+    if (hideTime) {
+        setTimeout(() => setHide(true), hideTime)
+    }
+
+    if (!visible || hide) return null
+    return (
+        <Button
+            color={level === "success" ? "darkGreen" : "red"}
+            buttonStyle={styles.snackbar}
+            handlePress={handlePress}
+        >
+            {SNACKBAR_ICONS[level]}
+            <Text style={styles.text}>{text}</Text>
+        </Button>
+    )
+}
+
+export default Snackbar
---
+++ app/components/Snackbar/style.js
@@ -0,0 +1,27 @@
+import { StyleSheet } from "react-native"
+
+import COLORS from "@utils/colors"
+import FONTS from "@utils/fonts"
+
+const styles = StyleSheet.create({
+    snackbar: {
+        height: 50,
+        width: "90%",
+        position: "absolute",
+        flexDirection: "row",
+        top: 0,
+        borderRadius: 5,
+        justifyContent: "flex-start",
+        paddingLeft: 20,
+    },
+    text: {
+        fontFamily: FONTS.cairoBold,
+        color: COLORS.black,
+        fontSize: 16,
+        minHeight: 50,
+        textAlignVertical: "center",
+        marginLeft: 10,
+    },
+})
+
+export default styles
