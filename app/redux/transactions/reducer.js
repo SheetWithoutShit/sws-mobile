163b1647985ed93bd6594c7ba7854a97ff09bfe2
PetrushynskyiOleksii <petrushynskyi.oleksii@gmail.com>
2020-12-12 17:31:02 +0200

    SLM-90: Implement month budget report api logic

--- app/redux/transactions/reducer.js
+++ app/redux/transactions/reducer.js
@@ -3,12 +3,15 @@ import { actionType } from "@redux/transactions/actions"
 
 const initialState = {
     transactions: [],
+    monthReport: [],
 }
 
 export const reducer = (state = initialState, action) => {
     switch (action.type) {
     case actionType.SET_TRANSACTIONS:
         return { ...state, transactions: action.transactions }
+    case actionType.SET_MONTH_REPORT:
+        return { ...state, monthReport: action.monthReport }
     }
 
     return state
